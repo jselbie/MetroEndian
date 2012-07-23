fb1f20a2bd65115c8b0eb65ff31a9b18f0a45d16
John Selbie <jselbie@gmail.com>
2012-07-23 02:27:16 -0700

    first checkin

---
+++ nbo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+interface EndianConverter
+{
+    short ConvertShort(short x);
+    int ConvertInt(int x);
+    long ConvertLong(long x);
+}
+
+class ForBigEndian : EndianConverter
+{
+    public short ConvertShort(short x) { return x; }
+    public int ConvertInt(int x) { return x; }
+    public long ConvertLong(long x) { return x; }
+}
+
+
+class ForLittleEndian : EndianConverter
+{
+    public short ConvertShort(short x)
+    {
+        int x1 = (x >> 8) & 0x00ff;
+        int x2 = x  & 0x00ff;
+        int result = x1 | (x2 << 8);
+        return (short)result;
+    }
+
+    public int ConvertInt(int x)
+    {
+        int x1 = (x >> 24) & 0x00ff;
+        int x2 = (x >> 16) & 0x00ff;
+        int x3 = (x >> 8) & 0x00ff;
+        int x4 = x & 0x00ff;
+        int result = x1 | (x2 << 8) | (x3 << 16) | (x4 << 24);
+        return result;
+    }
+
+    public long ConvertLong(long x)
+    {
+        long x1 = (x >> 56) & 0x00ff;
+        long x2 = (x >> 48) & 0x00ff;
+        long x3 = (x >> 40) & 0x00ff;
+        long x4 = (x >> 32) & 0x00ff;
+        long x5 = (x >> 24) & 0x00ff;
+        long x6 = (x >> 16) & 0x00ff;
+        long x7 = (x >> 8) & 0x00ff;
+        long x8 = x & 0x00ff;
+        long result = x1 | (x2 << 8) | (x3 << 16) | (x4 << 24) | (x5 << 32) | (x6 << 40) | (x7 << 48) | (x8 << 56);
+        return result;
+    }
+}
+
+
+
+
+public static class NBO
+{
+    static public bool IsLittleEndian { get; private set; }
+    static EndianConverter _converter;
+
+
+#if NBO_USE_SELF_DETECT
+    static void SelfDetectLittleEndian()
+    {
+        System.IO.MemoryStream stream = new MemoryStream();
+        System.IO.BinaryWriter writer = new BinaryWriter(stream, new UTF8Encoding(), true);
+
+        uint value = 0x01020304;
+        int firstbyte = 0;
+        writer.Write(value);
+        writer.Dispose();
+        stream.Seek(0, SeekOrigin.Begin);
+        firstbyte = stream.ReadByte();
+        IsLittleEndian = (firstbyte == 0x04);
+        stream.Dispose();
+    }
+#endif
+
+
+    static NBO()
+    {
+#if NBO_USE_SELF_DETECT
+        SelfDetectLittleEndian();
+#else
+        IsLittleEndian = BitConverter.IsLittleEndian;
+#endif
+
+        if (IsLittleEndian)
+        {
+            _converter = new ForLittleEndian();
+        }
+        else
+        {
+            _converter = new ForBigEndian();
+        }
+
+    }
+
+    static public short ConvertShort(short x) { return _converter.ConvertShort(x); }
+    static public int ConvertInt(int x) { return _converter.ConvertInt(x); }
+    static public long ConvertLong(long x) { return _converter.ConvertLong(x); }
+
+    static public ushort ConvertUShort(ushort x) { return (ushort)_converter.ConvertShort((short)x); }
+    static public uint ConvertUInt(uint x) { return (uint)_converter.ConvertInt((int)x); }
+    static public ulong ConvertULong(ulong x) { return (ulong)_converter.ConvertLong((long)x); }
+
+    // overloaded versions. I really despise overloaded methods in general, because you can mistakenly call the wrong version without realizing it
+    // but someone might prefer using this class this way
+    static public short Convert(short x) { return _converter.ConvertShort(x); }
+    static public int Convert(int x) { return _converter.ConvertInt(x); }
+    static public long Convert(long x) { return _converter.ConvertLong(x); }
+
+    static public ushort Convert(ushort x) { return (ushort)_converter.ConvertShort((short)x); }
+    static public uint Convert(uint x) { return (uint)_converter.ConvertInt((int)x); }
+    static public ulong Convert(ulong x) { return (ulong)_converter.ConvertLong((long)x); }
+};
+
+
---
+++ unittest.cs
@@ -0,0 +1,84 @@
+﻿
+public class NBOUnitTest
+{
+    static public bool RunTest()
+    {
+
+        int[] shortinputs = { 0x0000, 0x0001, 0x0002, 0x007f, 0x0080, 0x0081, 0x1234, 0x7fff, 0x8000, 0x8001, 0x8002, 0xfedc, 0xffee, 0xffff };
+        int[] shortoutputs = { 0x0000, 0x0100, 0x0200, 0x7f00, 0x8000, 0x8100, 0x3412, 0xff7f, 0x0080, 0x0180, 0x0280, 0xdcfe, 0xeeff, 0xffff };
+
+        long[] intinputs = { 0x00000000, 0x00000001, 0x00000002, 0x12345678, 0x87654321, 0x99887766, 0xfffffffe, 0xffffffff };
+        long[] intoutputs = { 0x00000000, 0x01000000, 0x02000000, 0x78563412, 0x21436587, 0x66778899, 0xfeffffff, 0xffffffff };
+
+        ulong[] longinputs = { 0, 0x0000000000000001, 0x0000000000000002, 0x1234567812345678, 0x8765432187654321, 0xfffffffffffffffe, 0xffffffffffffffff };
+        ulong[] longoutputs = { 0, 0x0100000000000000, 0x0200000000000000, 0x7856341278563412, 0x2143658721436587, 0xfeffffffffffffff, 0xffffffffffffffff };
+
+
+        for (int i = 0; i < shortinputs.Length; i++)
+        {
+            short input = (short)shortinputs[i];
+            short expected = (short)shortoutputs[i];
+            short actual = NBO.ConvertShort(input);
+
+            if (actual != expected)
+            {
+                return false;
+            }
+
+            ushort uinput = (ushort)((short)shortinputs[i]);
+            ushort uexpected = (ushort)((short)shortoutputs[i]);
+            ushort uactual = NBO.ConvertUShort(uinput);
+
+            if (uactual != uexpected)
+            {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < intinputs.Length; i++)
+        {
+            int input = (int)intinputs[i];
+            int expected = (int)intoutputs[i];
+            int actual = NBO.ConvertInt(input);
+
+            if (actual != expected)
+            {
+                return false;
+            }
+
+            uint uinput = (uint)((int)intinputs[i]);
+            uint uexpected = (uint)((int)intoutputs[i]);
+            uint uactual = NBO.ConvertUInt(uinput);
+
+            if (uactual != uexpected)
+            {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < longinputs.Length; i++)
+        {
+            long input = (long)longinputs[i];
+            long expected = (long)longoutputs[i];
+            long actual = NBO.ConvertLong(input);
+
+            if (actual != expected)
+            {
+                return false;
+            }
+
+            ulong uinput = longinputs[i];
+            ulong uexpected = longoutputs[i];
+            ulong uactual = NBO.ConvertULong(uinput);
+
+            if (uactual != uexpected)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+};
+
